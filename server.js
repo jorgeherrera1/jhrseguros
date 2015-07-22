63bb7de8b038a4df166c88a0c4c3d7e709229e39
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2015-07-21 20:25:37 -0500

    Commented express support of static files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     app = express();
 
 app.set('port', (process.env.PORT || 8080));
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
 
 app.listen(app.get('port'), function () {
     console.log('JHRSeguros is running on port', app.get('port'));
